23dc4db9e4dc0952dea57a0ebd0801f4b9b8695e
Alex Onyeka <alexonyekasm@gmail.com>
2025-11-08 07:23:16 +0100

    Changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8660ad34b70cba2cbb25ce2f8e86f06b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "31323edcc4d3c5bc8b111e4b5d3b97ad",
+"flutter_bootstrap.js": "75847673a772b784cf76cd9f7f6e052b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
